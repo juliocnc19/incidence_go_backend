8a8686f69e56b5b3d586c8998bd5d8da77954bc0
juliocnc19 <juliocnietoc19@gmail.com>
2025-04-09 11:23:11 -0400

    [ADD] cambios en min de dto para cambio valido en app

--- dto/auth/register.dto.go
+++ dto/auth/register.dto.go
@@ -4,6 +4,6 @@ type RegisterUserDto struct {
 	FirstName string `json:"first_name" validate:"required"`
 	LastName  string `json:"last_name" validate:"required"`
 	Email     string `json:"email" validate:"required,email"`
-	Password  string `json:"password" validate:"required,min=8"`
+	Password  string `json:"password" validate:"required,min=6"`
 	Username  string `json:"username" validate:"required"`
 }
